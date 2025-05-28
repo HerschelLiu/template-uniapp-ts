6fca337e2d6538e9a175c2b3a7fa32ae503d4579
herschel <herschelwork@163.com>
2025-05-28 20:16:18 +0800

    feat: [store]添加Pinia状态管理及持久化支持

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import uviewPlus from 'uview-plus'
 import { createSSRApp } from 'vue'
 
 import App from './App.vue'
-
 import 'uno.css'
+import store from './store'
 
 export function createApp() {
   const app = createSSRApp(App)
 
+  app.use(store)
   app.use(uviewPlus)
 
   return {
