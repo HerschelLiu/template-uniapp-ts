72abc6f73d8fa8754fa1eca5e3ecaa183e7e2aa4
herschel <herschelwork@163.com>
2025-05-28 16:31:19 +0800

    chore: 添加 unplugin-auto-import 依赖并配置自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default async () => {
@@ -8,7 +9,35 @@ export default async () => {
 
   return defineConfig({
     envDir: './env',
-    plugins: [uni(), UnoCSS()],
+    plugins: [
+      uni(),
+      UnoCSS(),
+      AutoImport({
+        imports: [
+          'pinia',
+          'vue',
+          'uni-app',
+          {
+            validing: [['default', 'validing']]
+          },
+          {
+            '@/utils/uniProxy': [['default', 'upp']]
+          },
+          {
+            '@/settings': [['default', 'settings']]
+          },
+          {
+            '@/store': [['default', 'pinia']]
+          }
+        ],
+        dts: 'src/types/auto-import.d.ts',
+        dirs: ['src/store', 'src/hooks', 'src/utils', 'src/api/**', 'src/enum'],
+        vueTemplate: true,
+        eslintrc: {
+          enabled: true
+        }
+      })
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, './src')
