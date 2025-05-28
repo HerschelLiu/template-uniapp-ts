eb2b684691c14329f2df5348c766f6906b59e048
herschel <herschelwork@163.com>
2025-05-28 13:19:45 +0800

    chore: 添加类型定义依赖并更新配置
    添加 @types/node 和 @uni-helper/uni-types 依赖以增强类型支持
    更新 tsconfig.json 包含新的类型定义并配置 volar 插件
    优化 vite.config.ts 的路径别名和预处理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [uni()],
-});
+export default async () => {
+  return defineConfig({
+    envDir: './env',
+    plugins: [uni()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: `@use '@/styles/mixin.scss' as *;`
+        }
+      }
+    }
+  })
+}
