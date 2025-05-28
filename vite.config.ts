705c973b45f4131368a0ec3ade8074eedc6359bf
herschel <herschelwork@163.com>
2025-05-28 19:37:35 +0800

    chore: 添加vite-plugin-restart插件以自动重启配置变更
    添加vite-plugin-restart插件，当修改vite.config.ts文件时自动重启服务，无需手动重新运行

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Optimization from '@uni-ku/bundle-optimizer'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default async () => {
@@ -51,7 +52,11 @@ export default async () => {
         },
         logger: false
       }),
-      eslint()
+      eslint(),
+      ViteRestart({
+        // 通过这个插件，在修改vite.config.js文件则不需要重新运行也生效配置
+        restart: ['vite.config.ts']
+      })
     ],
     resolve: {
       alias: {
