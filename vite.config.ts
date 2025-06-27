cce926c522b8d6487213cb982bc1f845672f5fb9
herschel <herschelwork@163.com>
2025-06-27 10:40:35 +0800

    fix: 修复scss变量文件导入路径错误

--- vite.config.ts
+++ vite.config.ts
@@ -71,7 +71,7 @@ export default async ({ mode }) => {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import '@/styles/mixin.scss';@import "@/styles/_variable.scss;`
+          additionalData: `@import '@/styles/mixin.scss';@import "@/styles/variable.scss;`
         }
       }
     }
